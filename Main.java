3ab7d7856ec8b82c47bf8ee214012082c04021c5
reza <reza.khani1998@gmail.com>
2019-06-30 14:01:09 +0430

    add list

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("HEllO");
+        List<String> testList = Arrays.asList("Amir" , "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
+
     }
 }
