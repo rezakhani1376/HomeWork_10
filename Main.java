f0ac98d61ec7ecb32d7ba20eecc3b74a3f340ec3
reza <reza.khani1998@gmail.com>
2019-06-30 14:06:57 +0430

    print listMap

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> asList = Arrays.asList("Amir" , "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
+        List<String> asList = Arrays.asList("Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
+
+        Map<Integer, List<String>> listMap = asList.stream()
+                .collect(groupingBy(String::length));
+
+        System.out.println(listMap);
+        
 
     }
+
 }
