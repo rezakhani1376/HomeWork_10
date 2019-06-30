be0523ebc0f2be09cb6ab1fdf782d1a4dad7f12c
reza <reza.khani1998@gmail.com>
2019-06-30 14:11:08 +0430

    create ListMap_b

--- Main.java
+++ Main.java
@@ -2,18 +2,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> asList = Arrays.asList("Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali", "Davood", "Reza", "Mohsen");
-
-        Map<Integer, List<String>> listMap = asList.stream()
+        // a
+        Map<Integer, List<String>> listMap_A = asList.stream()
                 .collect(groupingBy(String::length));
 
-        System.out.println(listMap);
-        
+        System.out.println(listMap_A);
+        //b
+        Map<Integer, Long> ListMap_b = asList.stream()
+                .collect(groupingBy(String::length, counting()));
+        System.out.println(ListMap_b);
+
 
     }
 
